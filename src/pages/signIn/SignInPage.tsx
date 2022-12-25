310ace8a56899f6e6c7f2927b30a05fc9ef6a38b
王金涛 <wangjintao@wangjintaodeMacBook-Pro.local>
2022-12-25 15:26:59 +0800

    登录逻辑

--- src/pages/signIn/SignInPage.tsx
+++ src/pages/signIn/SignInPage.tsx
@@ -1,12 +1,13 @@
 import React, { FC } from 'react'
 import { UserLayout } from '../../layouts/userLayout';
+import { SignInForm } from './SignInForm';
 
 export const SignInPage: FC = (props) => {
   console.log(props);
 
   return (
     <UserLayout>
-      login
+      <SignInForm />
     </UserLayout>
   )
 }
