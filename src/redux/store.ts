a65697e58f4db0d27c994193969fccd174a6b6b9
王金涛 <wangjintao@wangjintaodeMacBook-Pro.local>
2022-12-25 15:30:15 +0800

    sign function

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { productDetailSlice } from './productDetail/slice'
 import { productSearchSlice } from './productSearch/slice'
+import { userSlice } from './user/slice'
 
 const rootReducer = combineReducers({
   productDetail: productDetailSlice.reducer,
-  productSearch: productSearchSlice.reducer
+  productSearch: productSearchSlice.reducer,
+  user: userSlice.reducer,
 })
 // const store = createStore(rootReducers, applyMiddleware(thunk))
 const store = configureStore({
